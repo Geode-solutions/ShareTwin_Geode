8918cacfdd3ef9007d7de22d362767c4e7524ab5
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-09-23 16:11:20 +0200

    stiemap

--- app.py
+++ app.py
@@ -53,6 +53,21 @@ SSL = app.config.get('SSL')
 
 app.register_blueprint(blueprint_geode.geode_routes)
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = flask.url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+
 flask_cors.CORS(app, origins=ORIGINS)
 
 # ''' Main '''
