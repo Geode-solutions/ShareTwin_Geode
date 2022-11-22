22ef3470b4bc9bc93b4235f06469202f24a363b1
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-11-22 15:36:48 +0100

    feat(wip) : Get routes from dynamic_files branch

--- blueprint_geode.py
+++ blueprint_geode.py
@@ -34,10 +34,37 @@ def ping():
         f = open(LOCK_FOLDER + '/ping.txt', 'a')
         f.close()
     return flask.make_response({"message": "Flask server is running"}, 200)
+
 @geode_routes.route('/uploadfile', methods=['POST'])
 def uploadfile():
     try:
+        DATA_FOLDER = flask.current_app.config['DATA_FOLDER']
+        file = flask.request.form.get('file')
+        filename = flask.request.form.get('filename')
+        filesize = flask.request.form.get('filesize')
+        
+        if file is None:
+            return flask.make_response({"error_message": "No file sent"}, 400)
+        if filename is None:
+            return flask.make_response({"error_message": "No filename sent"}, 400)
+        if filesize is None:
+            return flask.make_response({"error_message": "No filesize sent"}, 400)
+        
+        newFileName = werkzeug.utils.secure_filename(filename)
+        uploadedFile = functions.UploadFile(file, newFileName, DATA_FOLDER, filesize)
+        if not uploadedFile:
+            flask.make_response({"error_message": "File not uploaded"}, 500)
+            
+        return flask.make_response({"newFilename": newFileName}, 200)
+    except Exception as e:
+        print("error : ", str(e))
+        return flask.make_response({"error_message": str(e)}, 500)
+
+@geode_routes.route('/convertfile', methods=['POST'])
+def convertfile():
+    try:
         UPLOAD_FOLDER = flask.current_app.config['UPLOAD_FOLDER']
+        DATA_FOLDER = flask.current_app.config['DATA_FOLDER']
         object = flask.request.form.get('object')
         file = flask.request.form.get('file')
         filename = flask.request.form.get('filename')
@@ -64,7 +91,7 @@ def uploadfile():
         if not uploadedFile:
             flask.make_response({"error_message": "File not uploaded"}, 500)
 
-        newFilePath = os.path.join(UPLOAD_FOLDER, newFileName)
+        newFilePath = os.path.join(DATA_FOLDER, newFileName)
         model = GeodeObjects.ObjectsList()[object]['load'](filePath)
         functions.GeodeObjects.ObjectsList()[object]['save'](model, newFilePath)
             
@@ -72,76 +99,3 @@ def uploadfile():
     except Exception as e:
         print("error : ", str(e))
         return flask.make_response({"error_message": str(e)}, 500)
-
-
-@geode_routes.route('/convertfile', methods=['POST'])
-async def convertfile():
-    try:
-        UPLOAD_FOLDER = flask.current_app.config['UPLOAD_FOLDER']
-        object = flask.request.form.get('object')
-        file = flask.request.form.get('file')
-        filename = flask.request.form.get('filename')
-        filesize = flask.request.form.get('filesize')
-        extension = flask.request.form.get('extension')
-
-        if object is None:
-            return flask.make_response({"error_message": "No object sent"}, 400)
-        if file is None:
-            return flask.make_response({"error_message": "No file sent"}, 400)
-        if filename is None:
-            return flask.make_response({"error_message": "No filename sent"}, 400)
-        if filesize is None:
-            return flask.make_response({"error_message": "No filesize sent"}, 400)
-        if extension is None:
-            return flask.make_response({"error_message": "No extension sent"}, 400)
-        
-        uploadedFile = functions.UploadFile(file, filename, UPLOAD_FOLDER, filesize)
-        if not uploadedFile:
-            flask.make_response({"error_message": "File not uploaded"}, 500)
-
-        secureFilename = werkzeug.utils.secure_filename(filename)
-        filePath = os.path.join(UPLOAD_FOLDER, secureFilename)
-        model = functions.GeodeObjects.ObjectsList()[object]['load'](filePath)
-        strictFileName = os.path.splitext(secureFilename)[0]
-        newFileName = strictFileName + '.' + extension
-
-        subFolder = f"{UPLOAD_FOLDER}/{strictFileName}/"
-        if os.path.exists(subFolder):
-            shutil.rmtree(subFolder)
-
-        functions.GeodeObjects.ObjectsList()[object]['save'](model, os.path.join(UPLOAD_FOLDER, newFileName))
-        mimetype = 'application/octet-binary'
-
-        list_exceptions = ['triangle', 'vtm']
-        if extension in list_exceptions:
-            if extension == 'triangle':
-                os.mkdir(subFolder)
-                generatedFiles = f"{UPLOAD_FOLDER}/{strictFileName}"
-                shutil.move(generatedFiles + '.ele', subFolder)
-                shutil.move(generatedFiles + '.neigh', subFolder)
-                shutil.move(generatedFiles + '.node', subFolder)
-            elif extension == 'vtm':
-                generatedFiles = f"{UPLOAD_FOLDER}/{strictFileName}"
-                shutil.move(generatedFiles + '.vtm', subFolder)
-            newFileName = strictFileName + '.zip'
-            mimetype = 'application/zip'
-            with zipfile.ZipFile(f'{UPLOAD_FOLDER}/{newFileName}', 'w') as zipObj:
-                    for folderName, subfolders, filenames in os.walk(subFolder):
-                        for filename in filenames:
-                            filePath = os.path.join(folderName, filename)
-                            zipObj.write(filePath, os.path.basename(filePath))
-
-        response = flask.send_from_directory(directory=UPLOAD_FOLDER, path=newFileName, as_attachment=True, mimetype = mimetype)
-        response.headers['new-file-name'] = newFileName
-        response.headers['Access-Control-Expose-Headers'] = 'new-file-name'
-        return response
-    except FileNotFoundError:
-        return flask.make_response({"error_message": "File not found"}, 404)
-    except RuntimeError as e:
-        return flask.make_response({"error_message": str(e)}, 500)
-    except Exception as e:
-        print("error : ", str(e))
-        return flask.make_response({"error_message": str(e)}, 500)
-
-
-    
