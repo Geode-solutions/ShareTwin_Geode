c690f85f650e2dc34ebbec56e3fce85830734947
JulienChampagnol <julien.champagnol@geode-solutions.com>
2022-11-29 17:06:55 +0100

    feat(wip) : Switch to snake_case

--- blueprint_geode.py
+++ blueprint_geode.py
@@ -4,6 +4,7 @@ import os
 import functions
 import werkzeug
 import GeodeObjects
+import uuid4
 
 geode_routes = flask.Blueprint('geode_routes', __name__)
 flask_cors.CORS(geode_routes)
@@ -20,7 +21,7 @@ def teardown_request(exception):
 def root():
     return flask.make_response({"message": "root"}, 200)
 @geode_routes.route('/sharetwin/createbackend', methods=['POST', 'OPTIONS'])
-def createbackend():
+def create_backend():
     return flask.make_response({"ID": str("123456")}, 200)
 @geode_routes.route('/healthcheck', methods=['GET'])
 def healthcheck():
@@ -36,37 +37,40 @@ def ping():
     return flask.make_response({"message": "Flask server is running"}, 200)
 
 @geode_routes.route('/uploadfile', methods=['POST'])
-def uploadfile():
+def upload_file():
     try:
+        ID = str(uuid.uuid4()).replace('-', '')
         DATA_FOLDER = flask.current_app.config['DATA_FOLDER']
         file = flask.request.form.get('file')
-        filename = flask.request.form.get('filename')
-        filesize = flask.request.form.get('filesize')
+        file_name = flask.request.form.get('filename')
+        file_size = flask.request.form.get('filesize')
         
         if file is None:
             return flask.make_response({"error_message": "No file sent"}, 400)
-        if filename is None:
-            return flask.make_response({"error_message": "No filename sent"}, 400)
-        if filesize is None:
+        if file_name is None:
+            return flask.make_response({"error_message": "No file_name sent"}, 400)
+        if file_size is None:
             return flask.make_response({"error_message": "No filesize sent"}, 400)
         
-        newFileName = werkzeug.utils.secure_filename(filename)
-        uploadedFile = functions.UploadFile(file, newFileName, DATA_FOLDER, filesize)
-        if not uploadedFile:
+        file_extension = os.path.splitext(file_name)[1]
+        new_file_name = ID + file_extension
+        uploaded_file = functions.upload_file(file, new_file_name, DATA_FOLDER, file_size)
+        if not uploaded_file:
             flask.make_response({"error_message": "File not uploaded"}, 500)
             
-        return flask.make_response({"newFilename": newFileName}, 200)
+        return flask.make_response({"newFilename": new_file_name, 'displayed_file_name': file_name}, 200)
     except Exception as e:
         print("error : ", str(e))
         return flask.make_response({"error_message": str(e)}, 500)
 
 @geode_routes.route('/convertfile', methods=['POST'])
-def convertfile():
+def convert_file():
     try:
+        ID = str(uuid.uuid4()).replace('-', '')
         UPLOAD_FOLDER = flask.current_app.config['UPLOAD_FOLDER']
         object = flask.request.form.get('object')
         file = flask.request.form.get('file')
-        filename = flask.request.form.get('filename')
+        file_name = flask.request.form.get('file_name')
         filesize = flask.request.form.get('filesize')
         extension = flask.request.form.get('extension')
 
@@ -74,27 +78,27 @@ def convertfile():
             return flask.make_response({"error_message": "No object sent"}, 400)
         if file is None:
             return flask.make_response({"error_message": "No file sent"}, 400)
-        if filename is None:
-            return flask.make_response({"error_message": "No filename sent"}, 400)
+        if file_name is None:
+            return flask.make_response({"error_message": "No file_name sent"}, 400)
         if filesize is None:
             return flask.make_response({"error_message": "No filesize sent"}, 400)
         if extension is None:
             return flask.make_response({"error_message": "No extension sent"}, 400)
         
-        secureFilename = werkzeug.utils.secure_filename(filename)
-        filePath = os.path.join(UPLOAD_FOLDER, secureFilename)
-        strictFileName = os.path.splitext(secureFilename)[0]
-        newFileName = strictFileName + '.' + extension
+        secure_file_name = werkzeug.utils.secure_filename(file_name)
+        file_path = os.path.join(UPLOAD_FOLDER, secure_file_name)
+        strict_file_name = os.path.splitext(secure_file_name)[0]
+        new_file_name = ID + '.' + extension
 
-        uploadedFile = functions.UploadFile(file, filename, UPLOAD_FOLDER, filesize)
-        if not uploadedFile:
+        uploaded_file = functions.upload_file(file, file_name, UPLOAD_FOLDER, filesize)
+        if not uploaded_file:
             flask.make_response({"error_message": "File not uploaded"}, 500)
 
-        newFilePath = os.path.join(UPLOAD_FOLDER, newFileName)
-        model = GeodeObjects.ObjectsList()[object]['load'](filePath)
+        newFilePath = os.path.join(UPLOAD_FOLDER, new_file_name)
+        model = GeodeObjects.ObjectsList()[object]['load'](file_path)
         functions.GeodeObjects.ObjectsList()[object]['save'](model, newFilePath)
             
-        return flask.make_response({"newFilename": newFileName}, 200)
+        return flask.make_response({"newFilename": new_file_name}, 200)
     except Exception as e:
         print("error : ", str(e))
         return flask.make_response({"error_message": str(e)}, 500)
--- functions.py
+++ functions.py
@@ -1,32 +1,32 @@
 import base64
-import GeodeObjects
+import geode_objects
 import os
 import pkg_resources
 import werkzeug
 import flask
 import uuid
 
-def ListAllInputExtensions():
+def list_all_input_extensions():
     """
     Purpose:
         Function that returns a list of all input extensions
     Returns:
         An ordered list of input file extensions
     """
-    List = []
-    ObjectsList = GeodeObjects.ObjectsList()
+    final_list = []
+    objects_list = geode_objects.objects_list()
 
-    for Object in ObjectsList.values():
-        values = Object['input']
+    for object_dict in objects_list.values():
+        values = object_dict['input']
         for value in values:
             list_creators = value.list_creators()
             for creator in list_creators:
-                if creator not in List:
-                    List.append(creator)
-    List.sort()
-    return List
+                if creator not in final_list:
+                    final_list.append(creator)
+    final_list.sort()
+    return final_list
 
-def ListObjects(extension: str):
+def list_objects(extension: str):
     """
     Purpose:
         Function that returns a list of objects that can handle a file, given his extension
@@ -35,19 +35,19 @@ def ListObjects(extension: str):
     Returns:
         An ordered list of object's names
     """
-    List = []
-    ObjectsList = GeodeObjects.ObjectsList()
+    final_list = []
+    objects_list = geode_objects.objects_list()
 
-    for Object, values in ObjectsList.items():
+    for Object, values in objects_list.items():
         list_values = values['input']
         for value in list_values:
             if value.has_creator(extension):
-                if Object not in List:
-                    List.append(Object)
-    List.sort()
-    return List
+                if Object not in final_list:
+                    final_list.append(Object)
+    final_list.sort()
+    return final_list
 
-def ListOutputFileExtensions(object: str):
+def list_output_file_extensions(object: str):
     """
     Purpose:
         Function that returns a list of output file extensions that can be handled by an object
@@ -56,45 +56,45 @@ def ListOutputFileExtensions(object: str):
     Returns:
         An ordered list of file extensions
     """
-    List = []
-    ObjectsList = GeodeObjects.ObjectsList()
+    final_list = []
+    objects_list = geode_objects.objects_list()
 
-    values = ObjectsList[object]['output']
+    values = objects_list[object]['output']
     for value in values:
         list_creators = value.list_creators()
         for creator in list_creators:
-            if creator not in List:
-                List.append(creator)
-    List.sort()
-    return List
+            if creator not in final_list:
+                final_list.append(creator)
+    final_list.sort()
+    return final_list
 
 
-def GetVersions(list_packages: list):
+def get_packages_versions(list_packages: list):
     list_with_versions = []
     for package in list_packages:
         list_with_versions.append({"package": package, "version": pkg_resources.get_distribution(package).version})
     return list_with_versions
 
-def UploadFile(file: str, filename: str, uploadFolder: str, filesize: int):
-    if not os.path.exists(uploadFolder):
-        os.mkdir(uploadFolder)
-    fileDecoded = base64.b64decode(file.split(',')[-1])
-    secureFilename = werkzeug.utils.secure_filename(filename)
-    filePath = os.path.join(uploadFolder, secureFilename)
-    f = open(filePath, "wb")
-    f.write(fileDecoded)
+def upload_file(file: str, file_name: str, upload_folder: str, file_size: int):
+    if not os.path.exists(upload_folder):
+        os.mkdir(upload_folder)
+    decoded_file = base64.b64decode(file.split(',')[-1])
+    secure_file_name = werkzeug.utils.secure_filename(file_name)
+    file_path = os.path.join(upload_folder, secure_file_name)
+    f = open(file_path, "wb")
+    f.write(decoded_file)
     f.close()
 
-    finalSize =  os.path.getsize(filePath)
-    return int(filesize) == int(finalSize)
+    final_size =  os.path.getsize(file_path)
+    return int(filesize) == int(final_size)
 
 def create_lock_file():
     LOCK_FOLDER = flask.current_app.config['LOCK_FOLDER']
     if not os.path.exists(LOCK_FOLDER):
         os.mkdir(LOCK_FOLDER)
     flask.g.UUID = uuid.uuid4()
-    filePath = f'{LOCK_FOLDER}/{str(flask.g.UUID)}.txt'
-    f = open(filePath, 'a')
+    file_path = f'{LOCK_FOLDER}/{str(flask.g.UUID)}.txt'
+    f = open(file_path, 'a')
     f.close()
 
 def remove_lock_file():
